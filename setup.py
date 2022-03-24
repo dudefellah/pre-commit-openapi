cd246f7f65c179ab520c0daf9b5421a4a9f223d8
Chris Wilcox <ckwilcox@gmail.com>
2022-03-24 12:01:51 -1000

    tweaks

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     name='pre_commit_dummy_package',
     version='0.0.0',
     install_requires=['openapi-spec-validator==0.2.9'],
+    python_requires='>=3',
     entry_points={
         'console_scripts': [
             'check-valid-openapi = pre_commit_hooks.check_valid_openapi:main',
